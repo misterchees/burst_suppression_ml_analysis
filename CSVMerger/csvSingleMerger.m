faacea1e07bf61adbccd20f9c571787bd613cda5
Jesus Nieto Reyes <jesusnietoreyes@yahoo.com>
2025-04-01 16:45:11 +0200

    Fixed csvSingleMerger, sollte jetzt auch die meisten edge cases von möglichen Fehlern abfangen

--- CSVMerger/csvSingleMerger.m
+++ CSVMerger/csvSingleMerger.m
@@ -7,33 +7,53 @@ function csvSingleMerger(folder1, folder2, output_folder, singleCSVName)
         mkdir(output_folder);
     end
 
+    % Variablen setzen
     filename = singleCSVName;
     file1_path = fullfile(folder1, filename);
     file2_path = fullfile(folder2, filename);
     output_path = fullfile(output_folder, filename);
 
-    % Datei aus Ordner1 einlesen
-    data1 = readtable(file1_path);
-    
-    % Überprüfen, ob zweite Spalte nicht nur aus 0 oder leeren Werten besteht
-    if any(data1(:,2) ~= 0 & ~isnan(data1(:,2)))
-        % Prüfen, ob Datei in Ordner2 existiert
-        if exist(file2_path, 'file')
-            data2 = readtable(file2_path);
-
-            % Prüfen, ob die Spaltenlängen übereinstimmen
-            if size(data1,1) ~= size(data2,1)
-                warning('Längendifferenz in Datei: %s (Differenz: %d)', filename, abs(size(data1,1) - size(data2,1)));
-            end
-            
-            % Zusammenführen der Daten
-            new_data = [data2, data1(:,2)];
-
-            % Neue CSV-Datei speichern
-            writetable(new_data, output_path);
-        else
-            warning('Datei %s existiert nicht in %s', filename, folder2);
-        end
+    % Prüfung ob Datei in Ordner1 existiert, sonst Abbruch
+    if ~exist(file1_path, 'file')
+        warning('Datei %s existiert nicht in %s', filename, folder1);
+        return
     end
+
+    % Datei aus Ordner1 einlesen
+    opts1 = detectImportOptions(file1_path, 'Delimiter', ','); % Trennzeichen auf komma setzen
+    data1 = readtable(file1_path, opts1);
+    
+    % Vorbereitung auf Überprüfung im folgenden Schritt
+    data1_col2 = data1{:, 2};  % Extrahiere zweite Spalte als numerischen Vektor
+    if iscell(data1_col2) % Umwandlung in numerisch falls als cell eingelesen
+        data1_col2 = str2double(data1_col2);
+    end
+
+    % Überprüfen, ob zweite Spalte nicht nur aus 0 oder leeren Werten
+    % besteht, sonst Abbruch
+    if ~any(data1_col2 ~= 0 & ~isnan(data1_col2))
+        warning('Es wurden keine MAC Werte in Datei %s gefunden. Datei wird übersprungen', filename)
+        return
+    end
+
+    % Prüfen, ob Datei in Ordner2 existiert, sonst Abbruch
+    if ~exist(file2_path, 'file')
+        warning('Datei %s existiert nicht in %s', filename, folder2);
+        return
+    end
+
+    opts2 = detectImportOptions(file1_path, 'Delimiter', ','); % Trennzeichen auf komma setzen
+    data2 = readtable(file2_path, opts2);
+
+    % Prüfen, ob die Spaltenlängen übereinstimmen
+    if size(data1,1) ~= size(data2,1)
+        warning('Längendifferenz in Datei: %s (Differenz: %d)', filename, abs(size(data1,1) - size(data2,1)));
+    end
+    
+    % Zusammenführen der Daten
+    new_data = [data2, data1(:,2)];
+
+    % Neue CSV-Datei speichern
+    writetable(new_data, output_path);
 end
 
--- CSVMerger/mergeWF.m
+++ CSVMerger/mergeWF.m
@@ -1,6 +1,10 @@
 folderMAC = 'C:\Users\jesus\OneDrive\Dokumente\Jesús\Studium\Fächer - Bioinformatik\Praktische Arbeit und Bachelorarbeit\Material\Daten\vitaldb_csv_anesthesia_mac';
 folderBIS = 'C:\Users\jesus\OneDrive\Dokumente\Jesús\Studium\Fächer - Bioinformatik\Praktische Arbeit und Bachelorarbeit\Material\Daten\vitaldb_csvprocessed_BIS_BIS_SR';
 output_folder = 'C:\Users\jesus\OneDrive\Dokumente\Jesús\Studium\Fächer - Bioinformatik\Praktische Arbeit und Bachelorarbeit\Material\Daten\vitaldb_csvprocessed_BIS_BIS_SR_MAC';
-singleCSV = '1.csv';
+mergerRange = 150:160;
 
-csvMerger(folderMAC, folderBIS, output_folder, singleCSV)
+for i = mergerRange
+    singleCSVString = strcat(i + ".csv");
+    singleCSV = convertStringsToChars(singleCSVString);
+    csvMerger(folderMAC, folderBIS, output_folder, singleCSV);
+end
